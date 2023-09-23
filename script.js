4989cc168148f517c30a7ef4b2b5b1e8f576935b
Avula Nagaraja <anagarajasfdc@gmail.com>
2023-09-23 11:20:20 +0530

    Name changed

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforesubmit(){
+    let outputdate  = document.querySelector('.outputdate');
+    let inputdate  = document.querySelector('.inputdate');
+    console.log('inputdate',inputdate.value);
+
+    let formatteddate = new Date(inputdate.value).toLocaleDateString();
+    outputdate.value = formatteddate();
+}
